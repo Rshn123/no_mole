b784f36867df9063bd69a721f81e9ba39f6b91a2
Pranaya Tamang <[200~workspace9823@gmail.com>
2024-12-18 20:03:50 +0545

    custom icons

--- No Mole/Components/CustomIcons.xaml.cs
+++ No Mole/Components/CustomIcons.xaml.cs
@@ -1,23 +1,58 @@
-﻿using System.Windows;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace No_Mole.Components
 {
+    /// <summary>
+    /// Interaction logic for CustomButton.xaml
+    /// </summary>
     public partial class CustomIcons : UserControl
     {
-
-        public static readonly DependencyProperty IconSourceProperty =
-            DependencyProperty.Register("IconSource", typeof(string), typeof(CustomIcons), new PropertyMetadata(null));
-
-        public string IconSource
-        {
-            get => (string)GetValue(IconSourceProperty);
-            set => SetValue(IconSourceProperty, value);
-        }
+        public event RoutedEventHandler? ButtonClick;
 
         public CustomIcons()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Raise the custom event when the button is clicked
+            ButtonClick?.Invoke(this, e);
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(string), typeof(CustomIcons),
+                new PropertyMetadata("/RecordImage.png"));
+        public string ImageSource
+        {
+            get
+            {
+                return (string)GetValue(ImageSourceProperty);
+            }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public string ToolTipText
+        {
+            get { return (string)GetValue(ToolTipTextProperty); }
+            set { SetValue(ToolTipTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToolTipTextProperty =
+            DependencyProperty.Register(nameof(ToolTipText), typeof(string), typeof(CustomIcons), new PropertyMetadata(string.Empty));
     }
 }
